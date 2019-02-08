4366c8a39573537dc56260304d09d7d6deb0e257
andy <zeusandninja@gmail.com>
2019-02-07 14:05:50 -1000

    made client not draw on socket emit but on its own interval

--- public/script.js
+++ public/script.js
@@ -11,6 +11,7 @@ $(function() {
         d : false
     }
     var movement;
+    var local_data;
     function down(e) {
         if(e.keyCode == 87 || e.keyCode == 38) {
             character.w = true;
@@ -42,15 +43,19 @@ $(function() {
         socket.emit("move", character);
     }
     socket.on("draw", function(data) {
+        local_data = data;
+    })
+
+    setInterval(function() {
         canvas.width=canvas.width;
-        for(var i = 0; i < data.heroes.length; i++) {
-            if(data.heroes[i].active == true) {
-                if(data.heroes[i].team == 0) {
+        for(var i = 0; i < local_data.heroes.length; i++) {
+            if(local_data.heroes[i].active == true) {
+                if(local_data.heroes[i].team == 0) {
                     ctx.fillStyle = "#1E90FF"
-                    ctx.fillRect(data.heroes[i].x, data.heroes[i].y, 20, 20)
+                    ctx.fillRect(local_data.heroes[i].x, local_data.heroes[i].y, 20, 20)
                 } else {
                     ctx.fillStyle = "#ED4337"
-                    ctx.fillRect(data.heroes[i].x, data.heroes[i].y, 20, 20)
+                    ctx.fillRect(local_data.heroes[i].x, local_data.heroes[i].y, 20, 20)
                 }
             }
         }
@@ -59,24 +64,24 @@ $(function() {
         ctx.moveTo(500, 0);
         ctx.lineTo(500, 500);
         ctx.stroke();
-        if(data.flags[0].on == "") {
+        if(local_data.flags[0].on == "") {
             ctx.strokeStyle = "#1E90FF"
-            ctx.strokeRect(100, 240, 20, 20); 
+            ctx.strokeRect(100, 240, 20, 20);
         } else {
             ctx.strokeStyle = "#1E90FF"
-            ctx.strokeRect(data.flags[0].on.x, data.flags[0].on.y, 20, 20); 
+            ctx.strokeRect(local_data.flags[0].on.x, local_data.flags[0].on.y, 20, 20);
         }
-        if(data.flags[1].on == "") {
+        if(local_data.flags[1].on == "") {
             ctx.strokeStyle = "#ED4337"
             ctx.strokeRect(870, 240, 20, 20);
         } else {
             ctx.strokeStyle = "#ED4337"
-            ctx.strokeRect(data.flags[1].on.x, data.flags[1].on.y, 20, 20); 
+            ctx.strokeRect(local_data.flags[1].on.x, local_data.flags[1].on.y, 20, 20);
         }
         ctx.font = "40px Ubuntu Mono";
         ctx.fillStyle = "blue";
-        ctx.fillText(data.score.blue, 470, 40);
+        ctx.fillText(local_data.score.blue, 470, 40);
         ctx.fillStyle = "red";
-        ctx.fillText(data.score.red, 510, 40);
-    })
+        ctx.fillText(local_data.score.red, 510, 40);
+    }, 1000/120)
 });
